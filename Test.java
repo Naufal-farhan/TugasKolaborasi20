467a2603fc094369dc2ea04a7f2b322aa82a0e54
Relln <farrel.novrizha@gmail.com>
2025-10-02 14:18:14 +0700

    test1

--- Test.java
+++ Test.java
@@ -2,7 +2,9 @@ public class Test{
     public static void main(String[] args) {
         
         System.out.println("Hi, ini baris pertama");
-    System.out.println("perbuhan ke dua icikiwr");
+        System.out.println("perbuhan ke dua icikiwr");
+        System.out.println("Hi, ini baris ketiga");
+        
     }
 
 }
