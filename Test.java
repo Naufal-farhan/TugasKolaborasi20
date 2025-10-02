05b2f8839f1980f7f86324bce56b430efbd6735b
Naufal-farhan <nefener123@gmail.com>
2025-10-02 14:17:23 +0700

    perbuhana ke2 nich

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test{
     public static void main(String[] args) {
         
         System.out.println("Hi, ini baris pertama");
+    System.out.println("perbuhan ke dua icikiwr");
     }
+
 }
