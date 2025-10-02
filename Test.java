e4274280223627038b48229f4e7ffff48d3b91ea
Naufal-farhan <nefener123@gmail.com>
2025-10-02 14:20:41 +0700

    perbuhaan ke 44

--- Test.java
+++ Test.java
@@ -4,7 +4,8 @@ public class Test{
         System.out.println("Hi, ini baris pertama");
         System.out.println("perbuhan ke dua icikiwr");
         System.out.println("Hi, ini baris ketiga");
-        
+        System.out.println("perbuhan ke 4 nih wir");
+        System.out.println("hmmmmm");
     }
 
 }
